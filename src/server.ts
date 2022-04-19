62cc75b7d78409d836c0d438844a646a37e93485
LuannMateus <luanmateuslima34@gmail.com>
2022-04-19 15:20:31 -0300

    feat: add project start

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import { app } from '.';
+
+dotenv.config();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
