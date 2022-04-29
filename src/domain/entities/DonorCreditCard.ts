16c23f6a47c447346d9eaa2fdd01e24e7b9eb4b6
LuannMateus <luanmateuslima34@gmail.com>
2022-04-29 16:30:33 -0300

    feat(entities): add DonorCreditCard entity and remove CreditCard entity

--- src/domain/entities/DonorCreditCard.ts
+++ src/domain/entities/DonorCreditCard.ts
@@ -1,11 +1,33 @@
 export default class DonorCreditCard {
     public id: string;
-    public ownerId: string;
-    public creditCardId: string;
 
-    constructor(id: string, ownerId: string, creditCardId: string) {
+    public creditCardTypeId: string;
+
+    public donorId: string;
+
+    public holderName: string;
+
+    public creditCardNumber: number;
+
+    public validity: number;
+
+    public cvv: number;
+
+    constructor(
+        id: string,
+        creditCardTypeId: string,
+        donorId: string,
+        holderName: string,
+        creditCardNumber: number,
+        validity: number,
+        cvv: number,
+    ) {
         this.id = id;
-        this.ownerId = ownerId;
-        this.creditCardId = creditCardId;
+        this.creditCardTypeId = creditCardTypeId;
+        this.donorId = donorId;
+        this.holderName = holderName;
+        this.creditCardNumber = creditCardNumber;
+        this.validity = validity;
+        this.cvv = cvv;
     }
 }
