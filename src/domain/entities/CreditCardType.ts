c2d3637aaf71f18a489f93de51a37b339c188ed0
LuannMateus <luanmateuslima34@gmail.com>
2022-04-29 11:18:57 -0300

    feat: add CreditCardType entity

---
+++ src/domain/entities/CreditCardType.ts
@@ -0,0 +1,13 @@
+export class CreditCardType {
+    public id: string;
+
+    public name: string;
+
+    public type: string;
+
+    constructor(id: string, name: string, type: string) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+}
